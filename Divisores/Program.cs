df81a30ecfd5f6a1ceea07c6d63e775a1a3fd6a1
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 14:59:12 -0300

    Add project files.

---
+++ Divisores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Divisores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número inteiro: ");
+            int x = int.Parse(Console.ReadLine());
+            
+            for(int i = 1; i <= x; i++)
+            {
+                if (x % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
